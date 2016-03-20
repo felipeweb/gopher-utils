d77f3316d4bf87a89e926f5178a8c4103f028a07
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-03-19 22:09:13 -0300

    fix date test on linux env

--- time_test.go
+++ time_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestDate(t *testing.T) {
 	Convey("Convert unix date format to string", t, func() {
-		So(Date(1, "DD/MM/YYYY"), ShouldEqual, "31/12/1969")
+		So(Date(1, "DD/MM/YYYY"), ShouldEqual, "01/01/1970")
 	})
 	Convey("Convert unix date in string format to humman string", t, func() {
-		So(DateS("1", "DD/MM/YYYY"), ShouldEqual, "31/12/1969")
+		So(DateS("1", "DD/MM/YYYY"), ShouldEqual, "01/01/1970")
 	})
 	Convey("Convert time object to string", t, func() {
-		So(DateT(time.Unix(int64(1), 0), "DD/MM/YYYY"), ShouldEqual, "31/12/1969")
+		So(DateT(time.Unix(int64(1), 0), "DD/MM/YYYY"), ShouldEqual, "01/01/1970")
 	})
 }
